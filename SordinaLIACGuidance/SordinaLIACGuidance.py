94e39dee449aa40b93d8879f9f220303f3972f3c
dgmato <dgmato@hggm.es>
2016-02-12 18:40:15 +0100

    Enh: Added up-down movement recording functionality.

--- SordinaLIACGuidance/SordinaLIACGuidance.py
+++ SordinaLIACGuidance/SordinaLIACGuidance.py
@@ -3,6 +3,9 @@ import unittest
 import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
 import logging
+import numpy
+import time, csv
+
 
 #
 # SordinaLIACGuidance
@@ -39,8 +42,6 @@ class SordinaLIACGuidanceWidget(ScriptedLoadableModuleWidget):
 
   def setup(self):
     ScriptedLoadableModuleWidget.setup(self)
-
-    logic = SordinaLIACGuidanceLogic()
 
     ########################################################
     ############# Transform Definition Area ################
@@ -167,6 +168,8 @@ class SordinaLIACGuidanceWidget(ScriptedLoadableModuleWidget):
     parametersFormLayout.addRow(self.loadModelsButton)
 
     # connections
+    self.applicatorToTrackerSelector.connect('currentNodeChanged(vtkMRMLNode*)',self.onSelect)
+    self.liacToTrackerSelector.connect('currentNodeChanged(vtkMRMLNode*)',self.onSelect)
     self.upDownRecordButton.connect('clicked(bool)', self.onUpDownRecordButton)
     self.upDownStopButton.connect('clicked(bool)', self.onUpDownStopButton)
     self.upDownRestartButton.connect('clicked(bool)', self.onUpDownRestartButton)
@@ -174,7 +177,8 @@ class SordinaLIACGuidanceWidget(ScriptedLoadableModuleWidget):
     self.rollStopButton.connect('clicked(bool)', self.onRollStopButton)
     self.rollRestartButton.connect('clicked(bool)', self.onRollRestartButton)
     
-    
+    self.SordinaLIACGuidanceLogic = SordinaLIACGuidanceLogic()  
+
     # Add vertical spacer
     self.layout.addStretch(1)
 
@@ -185,25 +189,36 @@ class SordinaLIACGuidanceWidget(ScriptedLoadableModuleWidget):
     pass
 
   def onSelect(self):
-    pass
+    self.applicatorToTrackerTransform=self.applicatorToTrackerSelector.currentNode()
+    self.liacToTrackerTransform=self.liacToTrackerSelector.currentNode()
 
   def onUpDownRecordButton(self):
     self.upDownRecordButton.enabled = False
     self.upDownStopButton.enabled = True
     self.upDownRestartButton.enabled = False
     
+    if not self.SordinaLIACGuidanceLogic.observedNode:
+      self.SordinaLIACGuidanceLogic.resetScene()
+      self.SordinaLIACGuidanceLogic.record=True
+      self.SordinaLIACGuidanceLogic.movementType='Up-Down'
+      self.SordinaLIACGuidanceLogic.removeUpdateObserver()
+      self.SordinaLIACGuidanceLogic.addUpdateObserver(self.liacToTrackerTransform)
 
   def onUpDownStopButton(self):
     self.upDownRecordButton.enabled = False
     self.upDownStopButton.enabled = False
     self.upDownRestartButton.enabled = True
-    
 
+    self.SordinaLIACGuidanceLogic.record=False
+    self.SordinaLIACGuidanceLogic.saveData('Up-Down')
+    self.SordinaLIACGuidanceLogic.observedNode=None
+    
   def onUpDownRestartButton(self):
     self.upDownRecordButton.enabled = True
     self.upDownStopButton.enabled = False
     self.upDownRestartButton.enabled = False
     
+    self.SordinaLIACGuidanceLogic.resetScene()
 
   def onRollRecordButton(self):
     self.rollRecordButton.enabled = False
@@ -225,101 +240,96 @@ class SordinaLIACGuidanceWidget(ScriptedLoadableModuleWidget):
 #
 
 class SordinaLIACGuidanceLogic(ScriptedLoadableModuleLogic):
-  """This class should implement all the actual
-  computation done by your module.  The interface
-  should be such that other python code can import
-  this class and make use of the functionality without
-  requiring an instance of the Widget.
-  Uses ScriptedLoadableModuleLogic base class, available at:
-  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
-  """
-
-  def hasImageData(self,volumeNode):
-    """This is an example logic method that
-    returns true if the passed in volume
-    node has valid image data
+  
+  def __init__(self): 
+    self.m = vtk.vtkMatrix4x4() # 4x4 VTK matrix to save the transformation matrix sent through Plus.
+    self.observedNode = None
+    self.outputObserverTag = -1
+    self.record = False
+    self.movementType = ''
+    self.timerActive=False
+    self.myTimer=Timer()
+    self.recordedDataBuffer = [] 
+    self.timeStamp = numpy.array([])
+    self.positionX = numpy.array([])
+    self.positionY = numpy.array([])
+    self.positionZ = numpy.array([])
+    
+  def resetScene(self):
+    self.m = vtk.vtkMatrix4x4() # 4x4 VTK matrix to save the transformation matrix sent through Plus.
+    self.observedNode = None
+    self.outputObserverTag = -1
+    self.record = False
+    self.movementType = ''
+    self.timerActive=False
+    self.myTimer=Timer()
+    self.recordedDataBuffer = [] 
+    self.timeStamp = numpy.array([])
+    self.positionX = numpy.array([])
+    self.positionY = numpy.array([])
+    self.positionZ = numpy.array([])
+      
+  def addUpdateObserver(self, inputNode):
     """
-    if not volumeNode:
-      logging.debug('hasImageData failed: no volume node')
-      return False
-    if volumeNode.GetImageData() == None:
-      logging.debug('hasImageData failed: no image data in volume node')
-      return False
-    return True
-
-  def isValidInputOutputData(self, inputVolumeNode, outputVolumeNode):
-    """Validates if the output is not the same as input
+    Summary: Add update observer to inputNode.
     """
-    if not inputVolumeNode:
-      logging.debug('isValidInputOutputData failed: no input volume node defined')
-      return False
-    if not outputVolumeNode:
-      logging.debug('isValidInputOutputData failed: no output volume node defined')
-      return False
-    if inputVolumeNode.GetID()==outputVolumeNode.GetID():
-      logging.debug('isValidInputOutputData failed: input and output volume is the same. Create a new volume for output to avoid this error.')
-      return False
-    return True
+    self.observedNode = inputNode
+    if self.outputObserverTag == -1:
+      self.outputObserverTag = inputNode.AddObserver('ModifiedEvent', self.updateSceneCallback)
 
-  def takeScreenshot(self,name,description,type=-1):
-    # show the message even if not taking a screen shot
-    slicer.util.delayDisplay('Take screenshot: '+description+'.\nResult is available in the Annotations module.', 3000)
+  def removeUpdateObserver(self):
+    """
+    Summary: Remove update observer.
+    """
+    if self.outputObserverTag != -1:
+      self.observedNode.RemoveObserver(self.outputObserverTag)
+      self.outputObserverTag = -1
+      self.observedNode = None
 
-    lm = slicer.app.layoutManager()
-    # switch on the type to get the requested window
-    widget = 0
-    if type == slicer.qMRMLScreenShotDialog.FullLayout:
-      # full layout
-      widget = lm.viewport()
-    elif type == slicer.qMRMLScreenShotDialog.ThreeD:
-      # just the 3D window
-      widget = lm.threeDWidget(0).threeDView()
-    elif type == slicer.qMRMLScreenShotDialog.Red:
-      # red slice window
-      widget = lm.sliceWidget("Red")
-    elif type == slicer.qMRMLScreenShotDialog.Yellow:
-      # yellow slice window
-      widget = lm.sliceWidget("Yellow")
-    elif type == slicer.qMRMLScreenShotDialog.Green:
-      # green slice window
-      widget = lm.sliceWidget("Green")
+  def updateSceneCallback(self, modifiedNode, event=None): 
+    """
+    Summary: This functions is called when the observed node (to which an observer has been added) is modified.
+    """           
+    if self.record:
+        if self.timerActive==False:
+            self.myTimer.startTimer()
+            self.timerActive=True
+        self.recordPositionsForCalibration(self.observedNode,self.movementType)
+            
+  def recordPositionsForCalibration(self, InputTransformMatrix, movementType):
+    print('Recording...')
+    # Input tranformation node is saved into a VTK matrix 'm'
+    InputTransformMatrix.GetMatrixTransformToParent(self.m)
+    # VTK matrix containing the input data is tranformed into a ordinary numpy matrix
+    self.positionX = numpy.append(self.positionX, self.m.GetElement(0, 3))
+    self.positionY = numpy.append(self.positionY, self.m.GetElement(1, 3))
+    self.positionZ = numpy.append(self.positionZ, self.m.GetElement(2, 3))
+    
+    t=self.myTimer.getElapsedTime()
+    self.timeStamp=numpy.append(self.timeStamp, t) # Time stamp saved for each iteration.
+  
+  def saveData(self, movementType):
+    dateAndTime = time.strftime("_%Y-%m-%d_%H-%M-%S")
+    if movementType=='Up-Down':
+      path = slicer.modules.sordinaliacguidance.path.replace("SordinaLIACGuidance.py","") + 'SavedData/' + 'CalibrationData_UpDown_' + dateAndTime
+    elif movementType=='Roll':
+      path = slicer.modules.sordinaliacguidance.path.replace("SordinaLIACGuidance.py","") + 'SavedData/' + 'CalibrationData_Roll_' + dateAndTime
     else:
-      # default to using the full window
-      widget = slicer.util.mainWindow()
-      # reset the type so that the node is set correctly
-      type = slicer.qMRMLScreenShotDialog.FullLayout
+      print ('ERROR saveData method: non-valid movement type.')
+    # Save data to .csv 
+    self.writeRecordedDataBufferToFile(path + '.csv')  
+    
+  def writeRecordedDataBufferToFile(self, path): 
+    with open(path, 'wb') as csvfile:
+      writer = csv.writer(csvfile, delimiter=",")
+      writer.writerow(['timeStamp', 'x' , 'y', 'z'])
 
-    # grab and convert to vtk image data
-    qpixMap = qt.QPixmap().grabWidget(widget)
-    qimage = qpixMap.toImage()
-    imageData = vtk.vtkImageData()
-    slicer.qMRMLUtils().qImageToVtkImageData(qimage,imageData)
-
-    annotationLogic = slicer.modules.annotations.logic()
-    annotationLogic.CreateSnapShot(name, description, type, 1, imageData)
-
-  def run(self, inputVolume, outputVolume, imageThreshold, enableScreenshots=0):
-    """
-    Run the actual algorithm
-    """
-
-    if not self.isValidInputOutputData(inputVolume, outputVolume):
-      slicer.util.errorDisplay('Input volume is the same as output volume. Choose a different output volume.')
-      return False
-
-    logging.info('Processing started')
-
-    # Compute the thresholded output volume using the Threshold Scalar Volume CLI module
-    cliParams = {'InputVolume': inputVolume.GetID(), 'OutputVolume': outputVolume.GetID(), 'ThresholdValue' : imageThreshold, 'ThresholdType' : 'Above'}
-    cliNode = slicer.cli.run(slicer.modules.thresholdscalarvolume, None, cliParams, wait_for_completion=True)
-
-    # Capture screenshot
-    if enableScreenshots:
-      self.takeScreenshot('SordinaLIACGuidanceTest-Start','MyScreenshot',-1)
-
-    logging.info('Processing completed')
-
-    return True
+      for i in range(len(self.timeStamp)):
+        vector = numpy.array([self.timeStamp[i],\
+            self.positionX[i],\
+            self.positionY[i],\
+            self.positionZ[i]])
+        writer.writerow(vector)
 
 
 class SordinaLIACGuidanceTest(ScriptedLoadableModuleTest):
@@ -340,38 +350,39 @@ class SordinaLIACGuidanceTest(ScriptedLoadableModuleTest):
     self.setUp()
     self.test_SordinaLIACGuidance1()
 
-  def test_SordinaLIACGuidance1(self):
-    """ Ideally you should have several levels of tests.  At the lowest level
-    tests should exercise the functionality of the logic with different inputs
-    (both valid and invalid).  At higher levels your tests should emulate the
-    way the user would interact with your code and confirm that it still works
-    the way you intended.
-    One of the most important features of the tests is that it should alert other
-    developers when their changes will have an impact on the behavior of your
-    module.  For example, if a developer removes a feature that you depend on,
-    your test should break so they know that the feature is needed.
-    """
 
-    self.delayDisplay("Starting the test")
-    #
-    # first, get some data
-    #
-    import urllib
-    downloads = (
-        ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
-        )
+class Timer(object):
 
-    for url,name,loader in downloads:
-      filePath = slicer.app.temporaryPath + '/' + name
-      if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
-        logging.info('Requesting download %s from %s...\n' % (name, url))
-        urllib.urlretrieve(url, filePath)
-      if loader:
-        logging.info('Loading %s...' % (name,))
-        loader(filePath)
-    self.delayDisplay('Finished with download and loading')
-
-    volumeNode = slicer.util.getNode(pattern="FA")
-    logic = SordinaLIACGuidanceLogic()
-    self.assertTrue( logic.hasImageData(volumeNode) )
-    self.delayDisplay('Test passed!')
+  def __init__(self):
+    self.startTime = 0.0
+    self.stopTime = 0.0
+    self.timerStarted = False
+    
+  def startTimer(self):
+    if not self.timerStarted:      
+      self.startTime = time.clock()
+      if self.stopTime != 0.0:
+        self.stopTime = time.clock() - self.stopTime
+      self.timerStarted = True
+    else:
+      logging.warning('Timer already running')
+      
+  def stopTimer(self):
+    if self.timerStarted:
+      self.stopTime = time.clock()
+      self.timerStarted = False
+    else:
+      logging.warning('Timer not running')
+      
+  def getElapsedTime(self):
+    if self.startTime == 0.0:
+      return 0.0
+    elif self.stopTime == 0.0:
+      return time.clock() - self.startTime
+    else:
+      return time.clock() - (self.stopTime - self.startTime)
+        
+  def resetTimer(self):
+    if self.startTime != 0.0:
+      self.startTime = time.clock()    
+      self.stopTime = 0.0
